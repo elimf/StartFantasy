c7d9ca94f790d7c3f6a1195b4f78053aba6bb849
elimf <elim.florvil@icloud.com>
2021-11-10 15:58:20 +0100

    debut story 5

---
+++ game.java
@@ -0,0 +1,12 @@
+
+public class game {
+    public static void setGames() {
+        System.out.println("The game will start now!!!!");
+        
+    }
+    public static void displayWinner(String name, String type) {
+        System.out.println("The WINNER IS" +name );
+        
+    }
+    
+}
