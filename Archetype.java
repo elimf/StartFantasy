7bc2392800dad5298b7ef05bb38ca70b80b642fe
chalbrecht <75670040+chalbrecht@users.noreply.github.com>
2021-11-10 14:52:05 +0100

    story 3 create characters and take dmg

---
+++ Archetype.java
@@ -0,0 +1,36 @@
+public class Archetype {
+    
+    protected String name;
+    protected int damage;
+    protected int pv;
+    protected int initiative;
+
+    void takeDamage(int damage_receive){
+        pv -=damage_receive;
+    };
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+    public int getPv() {
+        return pv;
+    }
+    public void setPv(int pv) {
+        this.pv = pv;
+    }
+    public int getInitiative() {
+        return initiative;
+    }
+    public void setInitiative(int initiative) {
+        this.initiative = initiative;
+    }
+}
