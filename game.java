e12c080a21589adea33cccb017a752931ecc5ba7
elimf <elim.florvil@icloud.com>
2021-11-11 02:45:19 +0100

    mise en place de l'affichage du jeu

--- game.java
+++ game.java
@@ -2,6 +2,7 @@
 public class game {
     public static void setGames() {
         System.out.println("The game will start now!!!!");
+        create.display();
         
     }
     public static void displayWinner(String name, String type) {
