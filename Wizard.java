a721c1a463730c9987dd64ca28a9c654b13359d6
elimf <elim.florvil@icloud.com>
2021-11-12 11:23:02 +0100

    ajout du coup amplifié au Wizard

--- Wizard.java
+++ Wizard.java
@@ -4,16 +4,18 @@ public class Wizard extends Archetype{
         Archetype.damage = damageWizard;
         Archetype.pv = pvWizard;
         Archetype.initiative = initiativeWizard;
-        gen =nameWizard+" "+damageWizard+" "+pvWizard+" "+ initiativeWizard;
+        Archetype.damageAmplified =damageAmplifiedWizard;
+        gen =nameWizard+" "+damageWizard+" "+pvWizard+" "+ initiativeWizard+" "+damageAmplifiedWizard;
     }
 
     String nameWizard = "Wizard";
-    int damageWizard = 90;
-    int pvWizard = 200;
-    int initiativeWizard = 4;
+    int damageWizard = addDamage();
+    int pvWizard = addPv();
+    int initiativeWizard = addIniatiative();
+    boolean damageAmplifiedWizard= addDamageAmplified();
     String gen ;
     public String toString() {
-        return "  name : "+ nameWizard + " | Damage : " + damageWizard+ " /100 |  pv : " + pvWizard+ " /500 | initiative : " + initiativeWizard + "/10.";
+        return "  name : "+ nameWizard + " | Damage : " + damageWizard+ " /100 |  pv : " + pvWizard+ " /500 | initiative : " + initiativeWizard + "/10 | Damage Amplified :"+ damageAmplifiedWizard;
     }
     
 }
