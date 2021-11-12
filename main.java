6f1ce5adcb20ebafac663daab47f137fe27c01ac
chalbrecht <75670040+chalbrecht@users.noreply.github.com>
2021-11-12 10:48:47 +0100

    characters ok but no esquive for thief

--- main.java
+++ main.java
@@ -3,10 +3,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Application has Started");
 
-        Warrior perso1 = new Warrior("Mathias", 8 , 15 ,99 );
-        Wizard perso2 = new Wizard("Gabriel", 9 , 15 ,74 );
+        Warrior perso1 = new Warrior("Mathias", 8 , 15 , 99 );
+        Wizard perso2 = new Wizard("Gabriel", 9 , 15 , 74 );
+        Thief perso3 = new Thief ("Thomas", 7 , 15 , 82);
 
         System.out.println(perso1);
         System.out.println(perso2);
+        System.out.println(perso3);
+        
     }
 }
